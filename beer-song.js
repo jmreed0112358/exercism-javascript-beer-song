e795455e7b0d5e2e7e76ef6205e9de3a5c96d08e
Jeremy M. Reed <reeje76@gmail.com>
2016-08-13 17:43:45 -0600

    Implement validateVerse

--- beer-song.js
+++ beer-song.js
@@ -7,16 +7,16 @@ BeerSong.prototype.verse = function(verseNum) {
   throw new NotImplementedException();
 };
 
-BeerSong.prototype.sing = function(startLine, endLine) {
+BeerSong.prototype.sing = function(startVerse, endVerse) {
   throw new NotImplementedException();
 };
 
-BeerSong.prototype.validateRange = function(startLine, endLine) {
+BeerSong.prototype.validateRange = function(startVerse, endVerse) {
   throw new NotImplementedException();
 };
 
 BeerSong.prototype.validateVerse = function(verseNum) {
-  throw new NotImplementedException();
+  return 0 <= verseNum && 99 >= verseNum;
 };
 
 module.exports = BeerSong;
