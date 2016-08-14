02bb05f06f69da6c961faf254b9389a1a4ca5d64
Jeremy M. Reed <reeje76@gmail.com>
2016-08-13 18:01:55 -0600

    Implement validateRange()
    Replace magic numbers with MIN and MAX constants.

--- beer-song.js
+++ beer-song.js
@@ -1,6 +1,9 @@
 var NotImplementedException = require('./exceptions/NotImplementedException.js'),
   InvalidParameterException = require('./exceptions/InvalidParameterException.js');
 
+const MIN = 0;
+const MAX = 99;
+
 var BeerSong = function() {};
 
 BeerSong.prototype.verse = function(verseNum) {
@@ -12,11 +15,15 @@ BeerSong.prototype.sing = function(startVerse, endVerse) {
 };
 
 BeerSong.prototype.validateRange = function(startVerse, endVerse) {
-  throw new NotImplementedException();
+  if (endVerse === undefined) {
+    return this.validateVerse(startVerse);
+  } else {
+    return (startVerse <= endVerse && this.validateVerse(startVerse) && this.validateVerse(endVerse));
+  }
 };
 
 BeerSong.prototype.validateVerse = function(verseNum) {
-  return 0 <= verseNum && 99 >= verseNum;
+  return MIN <= verseNum && MAX >= verseNum;
 };
 
 module.exports = BeerSong;
